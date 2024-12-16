dbbc0ce7f4ffd8d4ff0e2aec1501d3a9a47052e7
Henrique Silva <95050395+hlira44@users.noreply.github.com>
2024-12-16 16:45:58 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const schools = require('./escolas/schools.json');
+const schools = require('./escolas/data.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
